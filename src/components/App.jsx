c7e35a253f466fa46d4ed76a759946a5d2148f00
Oleksandr <107481048+RaccoonBLR@users.noreply.github.com>
2023-04-26 22:41:31 +0300

    update file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { lazy } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { SharedLayout } from './SharedLayout/SharedLayout';
@@ -21,19 +20,3 @@ function App() {
 }
 
 export default App;
-
-//  <div className="App">
-//       <header className="App-header">
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
