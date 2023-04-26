4f2a00bb340c3fd70adeefb7cd0c5e7895e0c8df
Oleksandr <107481048+RaccoonBLR@users.noreply.github.com>
2023-04-26 22:39:47 +0300

    update file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,18 @@ import { lazy } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { SharedLayout } from './SharedLayout/SharedLayout';
+import Home from 'pages/Home';
+import Tweets from 'pages/Tweets';
 
-const HomePage = lazy(() => import('../pages/Home'));
-const TweetsPage = lazy(() => import('../pages/Tweets'));
+// const HomePage = lazy(() => import('../pages/Home'));
+// const TweetsPage = lazy(() => import('../pages/Tweets'));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="/tweets" element={<TweetsPage />} />
+        <Route index element={<Home />} />
+        <Route path="/tweets" element={<Tweets />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
